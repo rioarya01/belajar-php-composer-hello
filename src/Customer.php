0c317da81a7078bace37d019345796584eac8166
rioarya01 <rio283931@gmail.com>
2024-02-06 18:43:10 +0800

    contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,12 @@
+<?php
+    namespace ProgrammerZamanNow\Belajar;
+
+    class Customer {
+        public function __construct(private string $name) {
+
+        }
+
+        public function sayHello(string $name):string {
+            return "Hello $name, My Name is $this->name";
+        }
+    }
